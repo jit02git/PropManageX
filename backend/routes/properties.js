d0b21996fa2a9327dca84ec55cb53cbb991bb9f1
jitesh <jitesh02sep@gmail.com>
2025-04-12 13:45:53 +0530

    minor changes

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getProperties,
-  getProperty,
-  createProperty,
-  updateProperty,
-  deleteProperty
-} = require('../controllers/propertyController');
+const { getProperties, getProperty, createProperty, updateProperty, deleteProperty } = require('../controllers/propertyController');
 
-router.route('/')
-  .get(getProperties)
-  .post(createProperty);
+router.route('/').get(getProperties).post(createProperty);
 
-router.route('/:id')
-  .get(getProperty)
-  .put(updateProperty)
-  .delete(deleteProperty);
+router.route('/:id').get(getProperty).put(updateProperty).delete(deleteProperty);
 
 module.exports = router;
